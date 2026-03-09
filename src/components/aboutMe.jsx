d2ad5918816d489c6dd368594b7c7c6d2252223b
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-03-09 07:16:11 +0100

    UI fixes

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -49,10 +49,10 @@ const aboutMe = (id) => {
           </h3>
           <div className=' xl:grid-cols-2 grid  w-full rounded-2xl gap-x-10   space-y-5 '>
             {projectsData.map((project) => (
-              <div className=' w-full xl:space-y-5 space-y-3 bg-white flex flex-col xl:p-5 p-3 xl:h-140 h-90 rounded-xl shadow-2xl' key={project.id}>
+              <div className=' max-w-full xl:space-y-5 space-y-3 bg-white flex flex-col  xl:p-5 p-3 xl:min-h-140 min-h-90 xl:h-140 h-100   rounded-xl shadow-2xl' key={project.id}>
                 <h2 className='font-bold xl:tracking-widest tracking-wide xl:text-xl text-xs' >{project.title}</h2>
                 <hr className='border-gray-400' />
-                <img src={project.image} alt="" className='rounded-md w-12/12 shadow-2xl ring-2 ring-white xl:h-70 h-50' />
+                <img src={project.image} alt="" className='rounded-md w-12/12 shadow-2xl ring-2 ring-white xl:min-h-70 min-h-50' />
                 {project.description.map((text, index) => (
                   <div key={index} className=' items-center flex space-x-2'>
                     <FontAwesomeIcon icon={faCheck} className='text-green-600' />
