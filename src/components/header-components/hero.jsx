74c6d3f2398b540713f84f5d08573887a1b6841a
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-04 15:10:07 +0100

    refactor: enhance layout structure and update developer titles in hero component

--- src/components/header-components/hero.jsx
+++ src/components/header-components/hero.jsx
@@ -6,8 +6,8 @@ const hero = () => {
   const About = [{ id: 1, name: 'View Projects', link: 'https://github.com/DanielApple05' }, { id: 2, name: 'Download CV', link: '/Daniel_Ejimofor_CV.pdf', download: true }];
   return (
     <div>
-      <div className='bg-[#0A0F1C] text-white xl:py-10 py-5 w-full xl:text-base text-xs place-content-center grid'>
-        <div className='flex justify-between xl:w-8/12 w-10/12 place-self-center '>
+      <div className='bg-[#0A0F1C] text-white xl:py-10 py-5 w-full xl:text-base text-xs grid'>
+        <div className='flex justify-between xl:w-5/12 w-10/12 place-self-center '>
           {Tabs.map((tab) => (
             tab.isSection ? (
               <a
@@ -27,21 +27,21 @@ const hero = () => {
             )
           ))}
         </div>
-        <div className='flex w-12/12 xl:px-20 px-5 xl:mt-10 mt-3 xl:pt-5 pt-2 xl:space-x-0 space-x-5 '>
-          <div className=' xl:w-4/12 w-3/12 justify-end flex items-center '>
+        <div className='flex w-full xl:px-20 px-5 xl:mt-10 mt-3 xl:pt-5 pt-2 xl:space-x-0 space-x-5 '>
+          {/* <div className=' xl:w-4/12 w-3/12 justify-end flex items-center '>
             <img src="/images/myIcon.jpeg" alt="My Icon" className='xl:w-60 w-20 xl:h-60 h-20 rounded-full object-cover xl:ring-5 ring-2 xl:m-2 m-1 ' />
-          </div>
-          <div className='flex flex-col place-items-center xl:space-y-7 space-y-2 xl:w-7/12 w-8/12 xl:pt-5 pt-2'>
+          </div> */}
+          <div className='flex flex-col place-items-start xl:space-y-7 space-y-2 xl:w-10/12 w-8/12 xl:pt-5 pt-2 '>
             <h1 className='xl:text-3xl text-xs font-bold'>
               Daniel Chukwuebuka Ejimofor
             </h1>
             <h3 className=' xl:text-xl text-xs font-semibold text-blue-600'>
-              <span className=' xl:mr-2 mr-1'>Frontend Developer</span> | <span className='xl:ml-2 ml-1'> React Developer</span>
+              <span className=' xl:mr-2 mr-1'>Full Stack Developer</span> | <span className='xl:ml-2 ml-1'> Mern Stack Developer</span>
             </h3>
             <p >
               I build responsive, user friendly web applications with React and modern UI tools.
             </p>
-            <div className='flex xl:space-x-18 space-x-5 place-self-center '>
+            <div className='flex xl:space-x-18 space-x-5'>
               {About.map((item) => (<button key={item.id} className='cursor-pointer bg-gray-400 hover:bg-blue-600 rounded-md xl:px-5 px-3 py-1 hover:ring-1 ' onClick={() => window.open(item.link, '_blank', 'noopener,noreferrer')}>
                 {item.name}
               </button>))}
