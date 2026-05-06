b9e3e14de1cc7a72a372dce0d819f0e3c4d945b8
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-06 15:35:54 +0100

    refactor: adjust indentation and formatting in AboutMe component for improved readability

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -93,15 +93,15 @@ const aboutMe = ({ id }) => {
               </div>
             ))}
           </div>
-          <div className=' flex place-self-center'>
-            <a href="https://github.com/DanielApple05"
-              target="_blank"
-              rel="noopener noreferrer"
-              className='flex place-self-center bg-blue-600 rounded p-2 xl:mt-6 mt-4 text-white cursor-pointer  xl:w-40 w-40 justify-center font-semibold text-center '>
-              View more projects
-            </a>
-          </div>
         </section>
+        <div className=' flex place-self-center'>
+          <a href="https://github.com/DanielApple05"
+            target="_blank"
+            rel="noopener noreferrer"
+            className='flex place-self-center bg-blue-600 rounded p-2 xl:mt-6 mt-4 text-white cursor-pointer  xl:w-40 w-40 justify-center font-semibold text-center '>
+            View more projects
+          </a>
+        </div>
       </div>
 
     </div>
