da8530c6ec50bc1e9cb68abbfe10172d22082222
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-15 21:22:40 +0100

    fix: update MobileNav width and add backdrop blur for improved aesthetics

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MobileNav = () => {
   return (
-    <div className='hidden:xl flex fixed bottom-10 mx-auto place-self-center space-x-5 bg-violet-800/50 w-7/12  shadow-2xs shadow-violet-700 p-3 rounded-full'>
+    <div className='hidden:xl flex fixed bottom-10 mx-auto place-self-center justify-around space-x-5 bg-violet-800/50 w-10/12  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs'>
       {
         Navs.map((nav) => (
           nav.isSection ? (
