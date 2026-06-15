d63f5f90172a3897214f408d31671d96c157dfbc
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-15 22:05:42 +0100

    fix: update MobileNav text size and improve NavLink active state styling

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -7,21 +7,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const MobileNav = () => {
   return (
     <div className='flex justify-center'>
-      <div className='hidden:xl flex fixed bottom-10 z-20 mx-auto justify-around bg-violet-600/50 w-60  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs text-sm'>
+      <div className='hidden:xl flex fixed bottom-10 z-20 mx-auto justify-around bg-violet-600/50 w-60  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs text-base'>
         {
           Navs.map((nav) => (
             nav.isSection ? (
               <a
                 key={nav.id}
                 href={`#${nav.id}`}
-                className='cursor-pointer hover:text-violet-800 translate'>
+                className='cursor-pointer hover:text-blue-400 hover:translate-y-0.5'>
                 <FontAwesomeIcon icon={nav.icon} />
               </a>
             ) : (
               <NavLink
                 key={nav.id}
                 to={nav.path}
-                className='cursor-pointer hover:text-blue-400'
+                className={({ isActive }) => `cursor-pointer hover:text-blue-400 hover:translate-y-0.5 ${ isActive ? 'bg-blue-400' : ''}`}
               >
                 <FontAwesomeIcon icon={nav.icon} />
               </NavLink>
