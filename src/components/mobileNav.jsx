673f89f4a4aec566f965de7ed469844e0b6947f8
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-15 21:45:27 +0100

    fix: format MobileNav component for improved readability

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -7,27 +7,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const MobileNav = () => {
   return (
     <div className='flex justify-center'>
-      <div className='hidden:xl flex fixed bottom-10 z-20 mx-auto justify-around bg-violet-800/50 w-60  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs text-sm'>
-      {
-        Navs.map((nav) => (
-          nav.isSection ? (
-            <a
-              key={nav.id}
-              href={`#${nav.id}`}
-              className='cursor-pointer hover:text-violet-800 translate'>
-              <FontAwesomeIcon icon={nav.icon} />
-            </a>
-          ) : (
-            <NavLink
-              key={nav.id}
-              to={nav.path}
-              className='cursor-pointer hover:text-blue-400'
-            >
-              <FontAwesomeIcon icon={nav.icon} />
-            </NavLink>
-          )
-        ))}
-    </div>
+      <div className='hidden:xl flex fixed bottom-10 z-20 mx-auto justify-around bg-violet-600/50 w-60  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs text-sm'>
+        {
+          Navs.map((nav) => (
+            nav.isSection ? (
+              <a
+                key={nav.id}
+                href={`#${nav.id}`}
+                className='cursor-pointer hover:text-violet-800 translate'>
+                <FontAwesomeIcon icon={nav.icon} />
+              </a>
+            ) : (
+              <NavLink
+                key={nav.id}
+                to={nav.path}
+                className='cursor-pointer hover:text-blue-400'
+              >
+                <FontAwesomeIcon icon={nav.icon} />
+              </NavLink>
+            )
+          ))
+        }
+      </div>
     </div>
   );
 }
