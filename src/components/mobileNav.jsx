53027dac7992210724483e26172521cb3a8267bd
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-15 21:41:47 +0100

    fix: update MobileNav layout and hover effects for improved aesthetics

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -6,14 +6,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MobileNav = () => {
   return (
-    <div className='hidden:xl flex fixed bottom-10 mx-auto place-self-center justify-around space-x-5 bg-violet-800/50 w-60  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs text-sm'>
+    <div className='flex justify-center'>
+      <div className='hidden:xl flex fixed bottom-10 z-20 mx-auto justify-around bg-violet-800/50 w-60  shadow-2xs shadow-violet-700 p-3 rounded-full backdrop-blur-xs text-sm'>
       {
         Navs.map((nav) => (
           nav.isSection ? (
             <a
               key={nav.id}
               href={`#${nav.id}`}
-              className='cursor-pointer hover:text-blue-400'>
+              className='cursor-pointer hover:text-violet-800 translate'>
               <FontAwesomeIcon icon={nav.icon} />
             </a>
           ) : (
@@ -26,6 +27,7 @@ const MobileNav = () => {
             </NavLink>
           )
         ))}
+    </div>
     </div>
   );
 }
