7df4a55738eab1d3df37bb3cc60a1ae88944f85d
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-16 10:37:48 +0100

    fix: update text size in hero component for improved readability and button styling

--- src/components/header-components/hero.jsx
+++ src/components/header-components/hero.jsx
@@ -32,15 +32,15 @@ const hero = () => {
           <h1 className='  text-3xl font-bold text-center'>
             Daniel Chukwuebuka Ejimofor
           </h1>
-          <h3 className=' xl:text-xl text-xs font-semibold text-blue-600'>
+          <h3 className=' text-sm font-semibold text-blue-600'>
             <span className=' xl:mr-2 mr-1'>Full Stack Developer</span> | <span className='xl:ml-2 ml-1'> Mern Stack Developer</span>
           </h3>
           <div className='flex xl:space-x-18 space-x-3'>
             {About.map((item) => (
-              <button key={item.id} className='hover:shadow-lg hover:shadow-violet-700/50 hover:translate-y-1 transition-all duration-300 cursor-pointer bg-blue-950 border border-violet-500/50 hover:bg-blue-600 rounded-md xl:px-5 px-3 py-1' 
-            onClick={() => window.open(item.link, '_blank', 'noopener,noreferrer')}>
-              {item.name}
-            </button>))}
+              <button key={item.id} className='hover:shadow-lg hover:shadow-violet-700/50 hover:translate-y-1 transition-all duration-300 cursor-pointer bg-blue-950 border border-violet-500/50 hover:bg-blue-600 rounded-md xl:px-5 px-3 py-1 text-lg'
+                onClick={() => window.open(item.link, '_blank', 'noopener,noreferrer')}>
+                {item.name}
+              </button>))}
           </div>
         </div>
       </div>
