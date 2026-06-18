4fe435c7dc7f9bbd8c0448a6f355d95ab333b3cc
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-18 15:24:20 +0100

    fix: enhance layout and functionality in SingleProject component with new icons and contact section

--- src/components/singleProject.jsx
+++ src/components/singleProject.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import projectsData from './projectsData';
 import { Link, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare, faCircleCheck, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpRightFromSquare, faCircleCheck, faArrowLeft, faPaperPlane, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const SingleProject = () => {
   const { id } = useParams();
@@ -18,7 +18,7 @@ const SingleProject = () => {
   }
 
   return (
-    <div className='min-h-screen bg-[#050816] text-slate-50 xl:p-20 p-5'>
+    <div className=' flex flex-col bg-[#050816] text-slate-50 xl:p-20 p-5'>
       <Link
         to="/" className='text-violet-500 hover:text-blue-500 hover:underline text-sm mb-8 inline-block'>
         <FontAwesomeIcon icon={faArrowLeft} /> Back to Projects
@@ -41,7 +41,7 @@ const SingleProject = () => {
               href={project.links.link}
               target='_blank'
               rel='noopener noreferrer'
-              className='font-bold py-2 px-6 flex items-center rounded-lg border border-blue-400 hover:bg-violet-500 bg-blue-700 hover:text-white hover:-translate-y-2 duration-300 transition-all'
+              className='font-bold py-2 px-6 flex items-center rounded-lg border border-blue-400 hover:bg-violet-500 bg-[#2563EB] hover:text-white hover:-translate-y-2 duration-300 transition-all'
             >
               {project.links.name}
             </a>
@@ -51,14 +51,14 @@ const SingleProject = () => {
           <img
             src={project.image}
             alt={project.title}
-            className='rounded-md  xl:h-full h-52 w-full object-cover shadow-2xl ring-2 ring-white '
+            className='rounded-md  xl:h-full h-52 w-full shadow-2xl ring-2 ring-white '
           />
         </div>
       </div>
-      <div className='flex xl:flex-row flex-col ring ring-blue-400 rounded-lg p-3 xl:min-h-56 min-h-auto'>
+      <div className='flex xl:flex-row flex-col border border-blue-400 rounded-lg p-3 xl:min-h-36 min-h-auto bg-[#0F172A]'>
         <div className=' flex flex-col xl:w-[30%] w-full items-center xl:border-x-2 border-y-2 xl:border-y-0 border-gray-400 p-3'>
           <p className='text-center font-semibold text-2xl xl:text-base text-violet-500 mb-3'>Technologies</p>
-          <div className=' xl:flex flex-row grid grid-cols-3 mb-3 xl:mb-0 items-center justify-between w-full '>
+          <div className=' grid xl:grid-cols-2 grid-cols-3  mb-3 xl:mb-0 items-center justify-between w-full '>
             {project.tools && project.tools.map((tool, index) => (
               <div key={index} className='flex mt-5 items-center' >
                 {tool.isAwesome
@@ -75,7 +75,7 @@ const SingleProject = () => {
             project.description && project.description.slice(0, 5).map((description) => (
               <div key={description.id} className='flex space-x-2 items-center  '>
                 <FontAwesomeIcon icon={faCircleCheck} className='text-violet-400 text-xl' />
-                <p className='mb-4 text-lg ' > {description}</p>
+                <p className='mb-2 text-lg ' > {description}</p>
               </div>
             ))
           }
@@ -103,6 +103,21 @@ const SingleProject = () => {
             <FontAwesomeIcon icon={project.gitRepo.icon} className='text-xl' />
           </a>
         </div>
+      </div>
+      <div className='bg-[#0F172A] flex justify-between mt-16 h-28 text-lg  items-center rounded-lg'>
+        <div className='flex items-center space-x-3 m-3 '>
+          <div>
+            <FontAwesomeIcon icon={faPaperPlane} className='text-3xl text-[#7C3AED]' />
+          </div>
+         <div>
+           <p className='mb-2'> Interested in worrking with us ?</p>
+          <p>I'm always open to discussing new opportunities and exciting projects.</p>
+         </div>
+        </div>
+        <button className=' flex items-center space-x-2 bg-[#7C3AED] rounded-l-lg p-2 cursor-pointer hover:translate-x-0.5 duration-300 transition-all hover:shadow-lg hover:shadow-violet-700/50 '>
+          <p>Get In Touch</p>
+          <FontAwesomeIcon icon={faArrowRight} className='text-lg' />
+        </button>
       </div>
     </div>
   );
