fe1fb05846ca3e14a61b36e94edc75c3b59df55e
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-09-20 05:26:14 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/header-components/hero.jsx
+++ src/components/header-components/hero.jsx
@@ -16,7 +16,7 @@ const hero = () => {
     "/images/Furniro_preview.PNG",
     "/images/BanleyRestaurant_preview.png",
   ];
-  const About = [{ id: 1, name: 'View Projects', link: 'https://github.com/DanielApple05' }, { id: 2, name: 'Download CV', link: '/Daniel_Ejimofor_CV.pdf', download: true }];
+  const About = [{ id: 1, name: 'View Projects', link: 'https://github.com/DanielApple05' }, { id: 2, name: 'Download CV', link: '/Daniel_Ejimofor_Resume.pdf', download: true }];
 
   useEffect(() => {
     const interval = setInterval(() => {
